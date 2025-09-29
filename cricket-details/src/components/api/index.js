bbf21a6a51ad0785580c817aca593b79eee23d96
Satyam <74068263+satyamgupta0103@users.noreply.github.com>
2025-09-30 04:00:47 +0530

    cherry-pick: Bug Fix 1

--- cricket-details/src/components/api/index.js
+++ cricket-details/src/components/api/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// This is a bug fix
 export const fetchAPI = async (apiUrl, cancelToken) => {
   const response = await axios.get(apiUrl, {
     cancelToken: cancelToken?.token,
