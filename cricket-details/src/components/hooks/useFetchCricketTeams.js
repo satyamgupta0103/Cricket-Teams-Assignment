17f16b33c962ec02343c95057436a808d25aa1ae
SATYAM GUPTA <74068263+satyamgupta0103@users.noreply.github.com>
2025-09-30 03:17:47 +0530

    task1: Create React custom hook to fetch API data (#1)

---
+++ cricket-details/src/components/hooks/useFetchCricketTeams.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import fetchAPI from "../API";
+
+const useCricketTeams = (apiUrl) => {
+  const [teams, setTeams] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let cancelToken;
+
+    const fetchTeams = async () => {
+      try {
+        setLoading(true);
+        cancelToken = axios.CancelToken.source();
+
+        const response = fetchAPI(apiUrl, cancelToken);
+
+        setTeams(response.data || []);
+        setError(null);
+      } catch (error) {
+        if (axios.isCancel(error)) {
+          return;
+        }
+        setError(error.message || "Something went wrong");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchTeams();
+
+    return () => {
+      cancelToken?.cancel();
+      // cancelToken = Safety mechanism to cancel API calls when they are no longer needed.
+    };
+  }, [apiUrl]);
+
+  return { teams, loading, error };
+};
+
+export default useCricketTeams;
---
+++ cricket-details/src/components/hooks/useTeamDetail.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import fetchAPI from "../API";
+
+const useTeamDetail = (apiURL) => {
+  const [teamDetail, setTeamDetail] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let cancelToken;
+
+    const fetchPlayer = async () => {
+      try {
+        setLoading(true);
+        cancelToken = axios.CancelToken.source();
+
+        const response = fetchAPI(apiUrl, cancelToken);
+
+        setTeamDetail(response.data || []);
+        setError(null);
+      } catch (error) {
+        if (axios.isCancel(error)) {
+          return;
+        }
+
+        setError(error.message || "Something went wrong");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPlayer();
+
+    return () => {
+      cancelToken?.cancel();
+    };
+  }, [apiURL]);
+
+  return { teamDetail, loading, error };
+};
+
+export default useTeamDetail;
